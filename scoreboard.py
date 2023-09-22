bf2a4601d65267d14a6205fd714e34b568b9a768
jasonqxkao <142387962+jasonqxkao@users.noreply.github.com>
2023-09-21 22:52:22 -0700

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -8,18 +8,24 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.speed("fastest")
         self.color("white")
         self.hideturtle()
         self.penup()
         self.goto(0, 270)
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Score: {self.score} High Score:{self.high_score}", align=ALIGNMENT, font=FONT)
 
     def change_score(self):
         self.clear()
         self.score += 1
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Score: {self.score} High Score:{self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.change_score()
